56c1f86e6c0d43d747e921116b6eb9ca586b57ad
michaellu88 <miclu88888@gmail.com>
2018-02-22 15:14:06 +0800

    change amazon ip

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "159.65.41.148:25905",
+  "54.201.125.90:25905",
   "159.65.47.173:25905"
 };
 
