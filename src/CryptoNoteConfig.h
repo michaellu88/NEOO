693299ffe0aa90570aab5c19efbebe7a70845019
michaellu88 <miclu88888@gmail.com>
2018-02-23 07:34:43 +0800

    ip update

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "54.201.125.90:25905",
+  "159.89.235.167:25905",
   "159.89.230.233:25905"
 };
 
